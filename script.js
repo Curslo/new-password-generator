a20375a404681e1af9337cc93fe3530a15c0554a
habert rogena <habertorogy12@gmail.com>
2024-07-24 14:18:54 +0300

    Random Password Generator Web App

---
+++ script.js
@@ -0,0 +1,21 @@
+// const passwordLength = 12;
+
+// const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+// const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+// const number = "0123456789";
+// const symbols = "!@#$%^&*()_+{}[]:;,.<>/?";
+
+// const allChars = upperCase + lowerCase + number + symbols;
+
+// function createPassword() {
+//   let password = "";
+//   password += upperCase[Math.floor(Math.random() * upperCase.length)];
+//   password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+//   password += number[Math.floor(Math.random() * number.length)];
+//   password += symbols[Math.floor(Math.random() * symbols.length)];
+
+//   while (passwordLength > password.length) {
+//     password += allChars[Math.floor(Math.random() * allChars.length)];
+//   }
+//   passwordBox.value = password;
+// }
